51c259464d97943df4d09895ebce16ebef4dc4ac
Timo Kähtävä <timo.paavola@gmail.com>
2018-03-01 14:07:16 +0200

    Prefabs, Skybox and BattleScene level design

---
+++ Assets/Sho/SeaRionStats.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sho
+{
+	public class SeaRionStats : MonoBehaviour
+	{
+		[SerializeField]
+		int hp = 0;
+		public int Hp
+		{
+			get
+			{
+				return hp;
+			}
+			private set
+			{
+				hp = value;
+			}
+		}
+
+
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
